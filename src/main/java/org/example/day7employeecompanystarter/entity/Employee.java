31f648fe10f0333b5ed2e308d1b634eaf22ef7b9
Vega Feng <vega.feng@oocl.com>
2025-09-09 18:14:03 +0800

    feat(day7): create a employee entity

---
+++ src/main/java/org/example/day7employeecompanystarter/entity/Employee.java
@@ -0,0 +1,20 @@
+package org.example.day7employeecompanystarter.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class Employee {
+    private long id;
+    private String name;
+    private int age;
+    private int salary;
+    private String gender;
+
+    public Employee(String name, int age, int salary, String gender) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.gender = gender;
+    }
+}
