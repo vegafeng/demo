b83e06f49793207c8546a70dbc7457dd9566b5f4
Vega Feng <vega.feng@oocl.com>
2025-09-09 18:11:34 +0800

    build(day7): create a springboot project

---
+++ src/main/java/org/example/day7employeecompanystarter/Day7EmployeeCompanyStarterApplication.java
@@ -0,0 +1,13 @@
+package org.example.day7employeecompanystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day7EmployeeCompanyStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day7EmployeeCompanyStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/day7employeecompanystarter/Day7EmployeeCompanyStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day7employeecompanystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day7EmployeeCompanyStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
