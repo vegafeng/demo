5f7d5d51773ea483d262954d658b47ba51ac525d
Vega Feng <vega.feng@oocl.com>
2025-09-09 19:41:26 +0800

    refactor(day7): rename starterApplication

--- src/main/java/org/example/demo/StarterApplication.java
+++ src/main/java/org/example/demo/StarterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Day7EmployeeCompanyStarterApplication {
+public class StarterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day7EmployeeCompanyStarterApplication.class, args);
+		SpringApplication.run(StarterApplication.class, args);
 	}
 
 }
