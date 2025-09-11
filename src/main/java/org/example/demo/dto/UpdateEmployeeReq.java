944cd95e0e67b9b81a054dca47992f873a331d2f
Vega Feng <vega.feng@oocl.com>
2025-09-11 18:34:54 +0800

    feat(day9): add UpdateEmployeeReq for change employee by specified type

---
+++ src/main/java/org/example/demo/dto/UpdateEmployeeReq.java
@@ -0,0 +1,31 @@
+package org.example.demo.dto;
+
+public class UpdateEmployeeReq {
+    private String name;
+    private String gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
