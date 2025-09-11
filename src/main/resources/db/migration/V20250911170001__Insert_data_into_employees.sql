18f2fd416db940453637e0666f3708a610b8e901
Vega Feng <vega.feng@oocl.com>
2025-09-11 20:46:47 +0800

    chore(day9): add sql file to test

---
+++ src/main/resources/db/migration/V20250911170001__Insert_data_into_employees.sql
@@ -0,0 +1,4 @@
+-- V20250911170001__Insert_data_into_employees.sql
+
+INSERT INTO `employees` (`age`, `company_id`, `gender`, `name`, `salary`, `status`) VALUES
+                                                                                        (25, 1, 'male', 'Tom', 5000, b'1');
