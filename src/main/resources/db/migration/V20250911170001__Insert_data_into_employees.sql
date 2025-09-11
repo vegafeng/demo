191dd56b918cd6f5aabca9701438ce0c9dfa4448
vegafeng <vega.feng@oocl.com>
2025-09-12 01:21:25 +0800

    feat(day9):add sql file

--- src/main/resources/db/migration/V20250911170001__Insert_data_into_employees.sql
+++
@@ -1,4 +0,0 @@
--- V20250911170001__Insert_data_into_employees.sql
-
-INSERT INTO `employees` (`age`, `company_id`, `gender`, `name`, `salary`, `status`) VALUES
-                                                                                        (25, 1, 'male', 'Tom', 5000, b'1');
---
+++ src/main/resources/db/migration/V20250912000000__Create_table_company.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS companies (
+                                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                         name VARCHAR(255) NOT NULL
+    );
