ebcb58366e133688b8af88956229a1c0c2692625
vegafeng <vega.feng@oocl.com>
2025-09-12 23:33:51 +0800

    refactor (day9): change table name

--- src/main/java/org/example/demo/entity/Company.java
+++ src/main/java/org/example/demo/entity/Company.java
@@ -16,7 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "companies")
+@Table(name = "tb_company")
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
